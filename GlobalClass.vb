25cf3e3aa1e4cacc9acf5b5596a081ff464c7b00
kioslampu <kioslampu88@gmail.com>
2025-07-13 13:07:17 +0700

    Add user management and configuration handling
    Updated `Invent2025.vbproj` to include `System.Data.SqlClient` and added an `<ImportedNamespaces>` element. Introduced `GlobalClass` with a nested `UserInfo` class for managing user credentials and database connections. Implemented configuration retrieval in `InventModule.vb` with error handling for application settings.

---
+++ GlobalClass.vb
@@ -0,0 +1,11 @@
+﻿Imports System.Data.SqlClient
+Public Class GlobalClass
+
+    Public Class UserInfo
+        Public Property Username As String
+        Public Property Password As String
+        Public Property AccessGranted As Boolean
+        Public Property Connection As SqlConnection
+    End Class
+
+End Class
---
+++ InventModule.vb
@@ -0,0 +1,41 @@
+﻿Imports System.Configuration
+Imports Microsoft
+
+
+
+Module InventModule
+
+    Dim strServer As String
+    Dim strDBName As String
+    Dim blnActivateLogin As Boolean
+    Dim strStoreName As String
+    Dim strCity As String
+    Dim strAddress As String
+    Dim strPhoneNumber As String
+    Dim strPrintLogo As String
+    Dim strPrinterName As String
+
+    ' Tambahkan class model user login
+
+
+    Private Sub GetProfileSetting()
+
+        Try
+            strServer = ConfigurationManager.AppSettings("ServerName")
+            strDBName = ConfigurationManager.AppSettings("DBName")
+            blnActivateLogin = Convert.ToBoolean(ConfigurationManager.AppSettings("ActivateLogin"))
+            strStoreName = ConfigurationManager.AppSettings("StoreName")
+            strCity = ConfigurationManager.AppSettings("City")
+            strAddress = ConfigurationManager.AppSettings("Address")
+            strPhoneNumber = ConfigurationManager.AppSettings("PhoneNumber")
+            strPrintLogo = ConfigurationManager.AppSettings("PrintLogo")
+            strPrinterName = ConfigurationManager.AppSettings("PrinterName")
+
+        Catch ex As Exception
+            MessageBox.Show("Gagal mengambil konfigurasi aplikasi: " & ex.Message,
+                        "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+
+    End Sub
+
+End Module
